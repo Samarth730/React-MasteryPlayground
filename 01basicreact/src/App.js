50815857d29ad51a6eea169dd206e5e462f79c1f
Samarth730 <ssamarth@gmail.com>
2024-11-19 14:33:43 +0530

    Initial setup for React project

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,22 @@
+import Paint from './paint.js'
+
+// if u use function componenet then you shoud write fName in Capitalize
+function App() {
+  return (
+    <>
+      
+        <Paint/>
+        <h1>Bro nedds debug</h1>
+    </>
+
+
+  );
+}
+
+
+export default App;
+
+
+//there is no difrence in create project in react app or vite 
+//basicaly vite is for thing handle in smothly that it
+
---
+++ 01basicreact/src/paint.js
@@ -0,0 +1,7 @@
+function Paint(){
+    return(
+
+        <h1>Samarth My Best Freind</h1>
+    )
+};
+export default Paint;
